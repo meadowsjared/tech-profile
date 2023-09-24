dee4441b3ff9cb75ef975a55417e3a9e5117124c
Jared Meadows <meadowsjared@gmail.com>
2023-09-24 00:36:59 -0700

    chore(userSlice): added title to UserState also exported userState interface for use in unit tests
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-interface UserState {
+export interface UserState {
   name: string;
+  title: string;
   email: string;
   githubLink: string;
   linkedInLink: string;
@@ -9,6 +10,7 @@ interface UserState {
 
 const initialState: UserState = {
   name: "Jared Meadows",
+  title: "Senior Software Developer | Frontend",
   email: "meadowsjared@gmail.com",
   githubLink: "https://github.com/meadowsjared",
   linkedInLink: "https://www.linkedin.com/in/jared-meadows/",
