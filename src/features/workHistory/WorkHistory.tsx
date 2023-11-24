9beaefd152cc1cd326498a97fcb6d5feaf9f04c2
Jared Meadows <meadowsjared@gmail.com>
2023-11-23 19:24:14 -0800

    feat: create WorkHistory component which uses all 5 job type components from previous commit
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/features/workHistory/WorkHistory.tsx
+++ src/features/workHistory/WorkHistory.tsx
@@ -1,9 +1,18 @@
+import JCSAndroidDeveloper from "./JCSAndroidDeveloper";
+import Journeyage from "./Journeyage";
+import Launchbadge from "./Launchbadge";
+import RSVisualBasic from "./RSVisualBasic";
+import RSWebDeveloper from "./RSWebDeveloper";
+import styles from "./WorkHistory.module.css";
+
 export default function WorkHistory() {
   return (
-    <main>
-      <div>
-        <p>Work History</p>
-      </div>
+    <main className={styles.main}>
+      <Journeyage />
+      <Launchbadge />
+      <RSWebDeveloper />
+      <RSVisualBasic />
+      <JCSAndroidDeveloper />
     </main>
   );
 }
