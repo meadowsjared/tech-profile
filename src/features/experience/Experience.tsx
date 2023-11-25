918ee866749b7f084099e92fd1c52e6fa1e91082
Jared Meadows <meadowsjared@gmail.com>
2023-11-24 20:55:10 -0800

    chore: revise experience with forgotten framework
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/features/experience/Experience.tsx
+++ src/features/experience/Experience.tsx
@@ -7,9 +7,9 @@ export default function Experience() {
       <div>
         With over a decade and a half of experience in front-end development, I
         have honed my skills in creating dynamic and responsive applications. My
-        expertise lies in Vue, Angular, and TypeScript, coupled with a strong
-        foundation in HTML, CSS, SCSS, and JavaScript. This diverse skill set
-        has enabled me to build robust and efficient web applications across
+        expertise lies in Vue, Angular, React, and TypeScript, coupled with a
+        strong foundation in HTML, CSS, SCSS, and JavaScript. This diverse skill
+        set has enabled me to build robust and efficient web applications across
         various domains. My work often involves integrating complex server-side
         technologies like AWS and NodeJS, ensuring seamless functionality.
         Additionally, I possess a solid understanding of UX principles, which
