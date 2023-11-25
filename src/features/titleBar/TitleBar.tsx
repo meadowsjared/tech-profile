cb027b3243842ff0e7409e226f00661167cf22f4
Jared Meadows <meadowsjared@gmail.com>
2023-11-25 12:02:13 -0800

    style: add position sticky to TitleBar with fancy border-radius transition 🍾
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/features/titleBar/TitleBar.tsx
+++ src/features/titleBar/TitleBar.tsx
@@ -2,21 +2,49 @@ import Link from "next/link";
 import styles from "./TitleBar.module.css";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 
-export default function TitleBar() {
+interface Props {
+  onStickyChange: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function TitleBar(props: Readonly<Props>) {
+  const [isSticky, setIsSticky] = useState(false);
+  const sentinelRef = useRef(null);
   const { email, githubLink, linkedInLink } = useSelector(
     (state: RootState) => state.user,
   );
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        props.onStickyChange(!entry.isIntersecting); // Update parent state
+        setIsSticky(!entry.isIntersecting);
+      },
+      { threshold: [1] },
+    );
+
+    if (sentinelRef.current) {
+      observer.observe(sentinelRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [props, sentinelRef]);
+
   return (
-    <div className={styles.main}>
-      <div className={styles.left}>
-        <Link href={linkedInLink}>LinkedIn</Link>·
-        <Link href={githubLink}>GitHub</Link>
+    <>
+      <div ref={sentinelRef}></div>
+      <div className={`${styles.main} ${isSticky ? styles.sticky : ""}`}>
+        <div className={styles.left}>
+          <Link href={linkedInLink}>LinkedIn</Link>·
+          <Link href={githubLink}>GitHub</Link>
+        </div>
+        <a className={styles.right} href={`mailto:${email}`}>
+          {email}
+        </a>
       </div>
-      <a className={styles.right} href={`mailto:${email}`}>
-        {email}
-      </a>
-    </div>
+    </>
   );
 }
